0df3ab053567a04ed73d8db4d1d25009e0c2bde4
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-03-19 10:47:02 -1200

    Title complete

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,8 +8,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="container">
-        <Title/>
+        <div className="container py-5">
+        <div className="row">
+          <div className="col-sm-8 offset-sm-2">
+          <Title/>
+          </div>
+        </div>
         </div>
       </div>
     );
--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,28 +1,77 @@
 import React, { Component } from 'react';
+import './Title.css';
 
 class Title extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            number: 0
+            title: 'This is a dummy title',
+            isInput: false
         }
     }
 
-    counter() {
+    editHandler() {
         this.setState({
-            number: this.state.number + 1
+            ...this.state,
+            isInput: true
         })
-        console.log(this.state.number);
+    }
 
+    inputChange(event) {
+        // console.log(event.target.value);
+        this.setState({
+            ...this.state,
+            title: event.target.value
+        })
+    }
+    keyPressHandler(event) {
+        if (event.key == 'Enter') {
+            this.setState({
+                ...this.state,
+                isInput: false
+            })
+        }
+
+    }
+    blurHandler(event){
+        this.setState({
+            ...this.state,
+            isInput: false
+        })
     }
 
     render() {
+        let output = null
+
+        if (this.state.isInput) {
+            output = (
+                <div className="Title">
+                    <input onChange={(event) => this.inputChange(event)}
+                        className="form-control titleinput"
+                        type="text"
+                        value={this.state.title}
+                        onKeyPress={(event) => this.keyPressHandler(event)}
+                        onBlur={event => this.blurHandler(event)}
+                        />
+                </div>
+            )
+        } else {
+            output = (
+                <div className="d-flex Title">
+
+                    <h1 className="display-4">{this.state.title}</h1>
+                    <span onClick={() => this.editHandler()} className="ml-auto edit-icon">
+                        <i className="fas fa-pencil-alt"></i>
+                    </span>
+                </div>
+            )
+        }
         return (
             <div>
-                <p>{this.state.number}</p>
-                <button className="btn btn-primary" onClick={() => this.counter()}>Click me</button>
+                {output}
             </div>
+
         )
 
     }
